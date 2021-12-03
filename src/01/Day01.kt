9fa58b98265c54144019a05d2cede2fc53dffaec
Ian Rainford <ian@rainfords.net>
2021-12-04 10:58:24 +1300

    Refactor

--- src/01/Day01.kt
+++ src/01/Day01.kt
@@ -1,3 +1,7 @@
+package `01`
+
+import readInputAsInts
+
 fun main() {
     fun part1(input: List<Int>): Int {
         return input.windowed(2).count { (a, b) -> a < b }
@@ -10,11 +14,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInputAsInts("Day01_test")
+    val testInput = readInputAsInts("01/Day01_test")
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInputAsInts("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInputAsInts("01/Day01")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
